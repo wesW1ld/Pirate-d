13f0f9741644d97363b49d776dbb8eff080c8943
Eric Nortmann <nortmanneric57@gmail.com>
2024-10-05 17:29:31 -0400

    Death nw

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{ 
+    public float speed; //can be changed in unity editor
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, speed);
+    }
+    
+    public void OnCollisionEnter2D(Collision2D other)
+    if (other.gameObject.CompareTag("Player"))
+    {
+        SceneManager.LoadScene("Retryscript");
+    }
+}
